91d95cf258e7e3e6751966e7f6c00dfd9f1feee7
Nikita <cloudfreze102@gmail.com>
2024-04-24 12:23:11 +0300

    Editing Chains

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,42 @@
 import path from 'path'
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  ssr: false,
-  app: {
-    head: {
-      title: "YUMAX",
-      link: [{ rel: "icon", type: "image/png", href: "/favicon.svg" }],
-    },
-  },
-  modules: [
-    "@pinia/nuxt",
-    "@vueuse/nuxt",
-    "@nuxtjs/tailwindcss",
-    "nuxt-primevue",
-    'nuxt-icon',
-    '@nuxtjs/i18n'
-  ],
-  css: ['~/assets/css/style.css', 'primeicons/primeicons.css'],
-  primevue: {
-    options: {
-      unstyled: true
-    },
-    importPT: { as: 'Wind', from: path.resolve(__dirname, './presets/wind/') },
-    cssLayerOrder: 'tailwind-base, primevue, tailwind-utilities'
-  },
-  runtimeConfig: {
-    public: {
-      baseUrl: 'https://api.yumaxpay.com/api'
-    }
-  },
-  i18n: {
-    lazy: true,
-    langDir: 'locales',
-    defaultLocale: 'en',
-    strategy: 'prefix_except_default',
-    locales: [
-      { code: 'en', iso: 'en-US', file: 'en.json', name: 'English' },
-      { code: 'ua', iso: 'uk-UA', file: 'ua.json', name: 'Українська' }
-    ]
-  },
-
+	devtools: { enabled: true },
+	ssr: false,
+	app: {
+		head: {
+			title: 'YUMAX',
+			link: [{ rel: 'icon', type: 'image/png', href: '/favicon.svg' }],
+		},
+	},
+	modules: [
+		'@pinia/nuxt',
+		'@vueuse/nuxt',
+		'@nuxtjs/tailwindcss',
+		'nuxt-primevue',
+		'nuxt-icon',
+		'@nuxtjs/i18n',
+	],
+	css: ['~/assets/css/style.css', 'primeicons/primeicons.css'],
+	primevue: {
+		options: {
+			unstyled: true,
+		},
+		importPT: { as: 'Wind', from: path.resolve(__dirname, './presets/wind/') },
+		cssLayerOrder: 'tailwind-base, primevue, tailwind-utilities',
+	},
+	runtimeConfig: {
+		public: {
+			baseUrl: 'https://api.yumaxpay.com/api',
+		},
+	},
+	i18n: {
+		lazy: true,
+		langDir: 'locales',
+		defaultLocale: 'en',
+		strategy: 'prefix_except_default',
+		locales: [
+			{ code: 'en', iso: 'en-US', file: 'en.json', name: 'English' },
+			{ code: 'ua', iso: 'uk-UA', file: 'ua.json', name: 'Українська' },
+		],
+	},
 })
--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
 import type { Chain } from '~/types/chain'
 import type { DataTableEditingRows } from 'primevue/datatable'
 definePageMeta({
-  middleware: 'user',
-  layout: 'default'
+	middleware: 'user',
+	layout: 'default',
 })
 
 const url = useRuntimeConfig().public.baseUrl
@@ -16,73 +16,107 @@ const selectedChain = ref<Chain | null>(null)
 const rowToEdit = ref<DataTableEditingRows>()
 
 const addChain = async () => {
-  console.log(user)
-  await $fetch(`${url}/Chains`, {
-    method: 'POST',
-    headers: { Authorization: `Bearer ${token.value}` },
-    body: { name: newChain.value, userId: user.value.id },
-    async onResponse({ response }) {
-      if (response.status === 201) {
-        newChain.value = ''
-        isAddChainDialogShown.value = false
-        await fetchChains()
-      }
-    }
-  })
+	console.log(user)
+	await $fetch(`${url}/Chains`, {
+		method: 'POST',
+		headers: { Authorization: `Bearer ${token.value}` },
+		body: { name: newChain.value, userId: user.value.id },
+		async onResponse({ response }) {
+			if (response.status === 201) {
+				newChain.value = ''
+				isAddChainDialogShown.value = false
+				await fetchChains()
+			}
+		},
+	})
 }
 
 const updateChain = async (chain: Chain) => {
-  await $fetch(`${url}/Chains/${chain.id}`, {
-    method: 'PUT',
-    headers: { Authorization: `Bearer ${token.value}` },
-    params: { id: chain.userId },
-    body: { name: chain.name, userId: chain.userId },
-    async onResponse({ response }) {
-      if (response.status === 204) {
-        await fetchChains()
-      }
-    }
-  })
+	await $fetch(`${url}/Chains/${chain.id}`, {
+		method: 'PUT',
+		headers: { Authorization: `Bearer ${token.value}` },
+		params: { id: chain.userId },
+		body: { name: chain.name, userId: chain.userId },
+		async onResponse({ response }) {
+			if (response.status === 204) {
+				await fetchChains()
+			}
+		},
+	})
 }
-
 
 await fetchChains()
 </script>
 
 <template>
-  <div>
-    <Header />
-    <section class="p-4">
-      <DataTable v-if="chains.length" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" :value="chains"
-        v-model:selection="selectedChain" v-model:editing-rows="rowToEdit" striped-rows selection-mode="single"
-        edit-mode="row" @row-edit-save="updateChain($event.newData)"
-        class="p-datatable-sm p-4 rounded-xl shadow-md bg-white">
-        <template #header>
-          <Button icon="pi pi-plus" rounded outlined :label="$t('global.add')" @click="isAddChainDialogShown = true" />
-        </template>
-        <Column field="id" header="ID" />
-        <Column field="name" :header="$t('global.chain')">
-          <template #body="{ data }">
-            <NuxtLink :to="`/chain-${data.id}`" class="text-green hover:underline transition">{{ data.name }}</NuxtLink>
-          </template>
-          <template #editor="{ data, field }">
-            <InputText v-model="data[field]" />
-          </template>
-        </Column>
-        <Column :header="$t('global.created')" field="createdAt">
-          <template #body="{ data }">
-            {{ useDateFormat(data.createdAt, 'DD.MM.YYYY').value }}
-          </template>
-        </Column>
-        <Column :rowEditor="true" style="width: 10%; min-width: 8rem" bodyStyle="text-align:center"></Column>
-      </DataTable>
-      <h1 v-else class="text-center text-green font-medium text-3xl">No chains found</h1>
-      <Dialog v-model:visible="isAddChainDialogShown" modal :header="$t('chain.new')" class="w-96">
-        <div class="flex flex-col gap-3">
-          <InputText v-model="newChain" class="w-full" :placeholder="$t('global.chain')" />
-          <Button :label="$t('global.add')" @click="addChain" />
-        </div>
-      </Dialog>
-    </section>
-  </div>
+	<div>
+		<Header />
+		<section class="p-4">
+			<DataTable
+				v-if="chains.length"
+				paginator
+				:rows="5"
+				:rowsPerPageOptions="[5, 10, 20, 50]"
+				:value="chains"
+				v-model:selection="selectedChain"
+				v-model:editing-rows="rowToEdit"
+				striped-rows
+				selection-mode="single"
+				edit-mode="row"
+				@row-edit-save="updateChain($event.newData)"
+				class="p-datatable-sm p-4 rounded-xl shadow-md bg-white"
+			>
+				<template #header>
+					<Button
+						icon="pi pi-plus"
+						rounded
+						outlined
+						:label="$t('global.add')"
+						@click="isAddChainDialogShown = true"
+					/>
+				</template>
+				<Column field="id" header="ID" />
+				<Column field="name" :header="$t('global.chain')">
+					<template #body="{ data }">
+						<NuxtLink
+							:to="`/chain-${data.id}`"
+							class="text-green hover:underline transition"
+							>{{ data.name }}</NuxtLink
+						>
+					</template>
+					<template #editor="{ data, field }">
+						<InputText v-model="data[field]" />
+					</template>
+				</Column>
+				<Column :header="$t('global.created')" field="createdAt">
+					<template #body="{ data }">
+						{{ useDateFormat(data.createdAt, 'DD.MM.YYYY').value }}
+					</template>
+				</Column>
+				<Column
+					:rowEditor="true"
+					style="width: 10%; min-width: 8rem"
+					bodyStyle="text-align:center"
+				></Column>
+			</DataTable>
+			<h1 v-else class="text-center text-green font-medium text-3xl">
+				No chains found
+			</h1>
+			<Dialog
+				v-model:visible="isAddChainDialogShown"
+				modal
+				:header="$t('chain.new')"
+				class="w-96"
+			>
+				<div class="flex flex-col gap-3">
+					<InputText
+						v-model="newChain"
+						class="w-full"
+						:placeholder="$t('global.chain')"
+					/>
+					<Button :label="$t('global.add')" @click="addChain" />
+				</div>
+			</Dialog>
+		</section>
+	</div>
 </template>
